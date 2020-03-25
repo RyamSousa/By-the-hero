d18b0aad789b9de8b91d4b771221bcf1c19ab042
RyamSousa <ryam.sousa123@gmail.com>
2020-03-25 17:40:43 -0300

    Front-end do app

--- BackEnd/src/controllers/incidentController.js
+++ BackEnd/src/controllers/incidentController.js
@@ -1,4 +1,4 @@
-const connection = require('../database/connection.js');
+const connection = require('../database/connection');
 
 module.exports = {
     
@@ -7,13 +7,17 @@ module.exports = {
 
         const [count] = await connection('incidents').count();
 
-        console.log(count);
-
         const incidents = await connection('incidents')
         .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
         .limit(5)
-        .offset(((page - 1 )* 5))
-        .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf']);
+        .offset((page - 1 ) * 5)
+        .select(['incidents.*', 
+        'ongs.name',
+        'ongs.email', 
+        'ongs.whatsapp',
+         'ongs.city', 
+        'ongs.uf'
+        ]);
 
         response.header('X-Total-Count', count['count(*)']);
         
